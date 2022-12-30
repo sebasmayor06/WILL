5831b137fb7f3ea7413f9371a5ed13be31576026
repositorio-henry <sebda06@gmail.com>
2022-12-30 01:34:21 -0500

    punto 2 terminado

--- 02.js
+++ 02.js
@@ -12,6 +12,13 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  var mayor = strings[0];
+  for (var i = 0; i < strings.length; i++){
+    if (strings[i].length > mayor.length) {
+       mayor = strings[i];
+    }
+  }
+  return mayor;
 }
 
 // No modifiques nada debajo de esta linea //
