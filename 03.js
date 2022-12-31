fb9cfa14746f5d75aa3f0390da6a004c917d0e74
repositorio-henry <sebda06@gmail.com>
2022-12-31 08:01:52 -0500

    punto 3 terminado

--- 03.js
+++ 03.js
@@ -14,6 +14,13 @@ function buscarAmigo(amigos, nombre) {
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
+
+  for (var i = 0; i < amigos.length; i++){
+    if (nombre === amigos[i].nombre) {
+      return amigos[i];
+        
+      }
+  }
   
 }
 
